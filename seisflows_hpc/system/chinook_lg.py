0e3b1a86c7f9674c45cdf699cc325161393de956
rmodrak <rmodrak@princeton.edu>
2017-01-20 23:18:45 -0500

    added chinook_lg

---
+++ seisflows_hpc/system/chinook_lg.py
@@ -0,0 +1,76 @@
+
+from os.path import abspath, basename, join
+
+import os
+import math
+import sys
+import time
+
+from seisflows.tools import msg
+from seisflows.tools import unix
+from seisflows.tools.code import call, findpath, saveobj
+from seisflows.tools.config import ParameterError, custom_import
+
+PAR = sys.modules['seisflows_parameters']
+PATH = sys.modules['seisflows_paths']
+
+
+class chinook_lg(custom_import('system', 'slurm_lg'):
+    """ Interface for University of Alaska Fairbanks chinook
+    """
+
+    def check(self):
+        """ Checks parameters and paths
+        """
+
+        # check parameters
+        if 'TITLE' not in PAR:
+            setattr(PAR, 'TITLE', basename(abspath('.')))
+
+        if 'WALLTIME' not in PAR:
+            setattr(PAR, 'WALLTIME', 30.)
+
+        if 'STEPTIME' not in PAR:
+            setattr(PAR, 'STEPTIME', 30.)
+
+        if 'SLEEPTIME' not in PAR:
+            setattr(PAR, 'SLEEPTIME', 1.)
+
+        if 'VERBOSE' not in PAR:
+            setattr(PAR, 'VERBOSE', 1)
+
+        if 'NTASK' not in PAR:
+            raise ParameterError(PAR, 'NTASK')
+
+        if 'NPROC' not in PAR:
+            raise ParameterError(PAR, 'NPROC')
+
+        if 'NODESIZE' not in PAR:
+            raise ParameterError(PAR, 'NODESIZE')
+
+        if 'SLURMARGS' not in PAR:
+            setattr(PAR, 'SLURMARGS', '')
+
+        # check paths
+        if 'SCRATCH' not in PATH:
+            setattr(PATH, 'SCRATCH', join(abspath('.'), 'scratch'))
+
+        if 'LOCAL' not in PATH:
+            setattr(PATH, 'LOCAL', None)
+
+        if 'SUBMIT' not in PATH:
+            setattr(PATH, 'SUBMIT', abspath('.'))
+
+        if 'OUTPUT' not in PATH:
+            setattr(PATH, 'OUTPUT', join(PATH.SUBMIT, 'output'))
+
+        if 'SYSTEM' not in PATH:
+            setattr(PATH, 'SYSTEM', join(PATH.SCRATCH, 'system'))
+
+
+
+    def mpiexec(self):
+        """ Specifies MPI exectuable; used to invoke solver
+        """
+        return 'mpirun -np %d ' % PAR.NPROC
+
