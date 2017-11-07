d7394e79952dd5e5bdae09ab967fe70afa3f2d20
Ryan Modrak <rmodrak@gmail.com>
2017-11-06 18:51:28 -0900

    updated chinook_sm

--- seisflows/system/chinook_sm.py
+++ seisflows/system/chinook_sm.py
@@ -7,7 +7,7 @@ import sys
 
 from os.path import abspath, basename, join
 from seisflows.tools import unix
-from seisflows.tools.code import call, findpath, saveobj
+from seisflows.tools.tools import call, findpath, saveobj
 from seisflows.config import ParameterError, custom_import
 
 PAR = sys.modules['seisflows_parameters']
@@ -25,59 +25,79 @@ class chinook_sm(custom_import('system', 'slurm_sm')):
     def check(self):
         """ Checks parameters and paths
         """
-
-        # check parameters
+        # name of job
         if 'TITLE' not in PAR:
             setattr(PAR, 'TITLE', basename(abspath('.')))
 
+        # time allocated for workflow in minutes
         if 'WALLTIME' not in PAR:
             setattr(PAR, 'WALLTIME', 30.)
-
-        if 'VERBOSE' not in PAR:
-            setattr(PAR, 'VERBOSE', 1)
-
-        if 'NPROC' not in PAR:
-            raise ParameterError(PAR, 'NPROC')
-
+         
+        # number of tasks
         if 'NTASK' not in PAR:
             raise ParameterError(PAR, 'NTASK')
 
+        # number of cores per task
+        if 'NPROC' not in PAR:
+            raise ParameterError(PAR, 'NPROC')
+    
+        # how to invoke executables
+        if 'MPIEXEC' not in PAR:
+            setattr(PAR, 'MPIEXEC', '') 
+    
+        # optional additional SLURM arguments
         if 'SLURMARGS' not in PAR:
             setattr(PAR, 'SLURMARGS', '')
 
+        # SLURM resource partition
         if 'PARTITION' not in PAR:
             setattr(PAR, 'PARTITION', 't1small')
 
-        # check paths
-        if 'SCRATCH' not in PATH:
-            setattr(PATH, 'SCRATCH', join(os.getenv('CENTER'), 'scratch', str(uuid4())))
+        # optional environment variable list VAR1=val1,VAR2=val2,...
+        if 'ENVIRONS' not in PAR:
+            setattr(PAR, 'ENVIRONS', '')
 
+        # level of detail in output messages
+        if 'VERBOSE' not in PAR:
+            setattr(PAR, 'VERBOSE', 1)
+
+        # where job was submitted
+        if 'WORKDIR' not in PATH:
+            setattr(PATH, 'WORKDIR', abspath('.'))
+
+        # where temporary files are written
+        if 'SCRATCH' not in PATH:
+            setattr(PATH, 'SCRATCH', join(os.getenv('CENTER1'), 'scratch', str(uuid4())))
+
+        # where output files are written
+        if 'OUTPUT' not in PATH:
+            setattr(PATH, 'OUTPUT', PATH.WORKDIR+'/'+'output')
+
+        # where system files are written
+        if 'SYSTEM' not in PATH:
+            setattr(PATH, 'SYSTEM', PATH.SCRATCH+'/'+'system')
+
+        # optional local scratch path
         if 'LOCAL' not in PATH:
             setattr(PATH, 'LOCAL', None)
-
-        if 'SUBMIT' not in PATH:
-            setattr(PATH, 'SUBMIT', abspath('.'))
-
-        if 'OUTPUT' not in PATH:
-            setattr(PATH, 'OUTPUT', join(PATH.SUBMIT, 'output'))
 
 
     def submit(self, workflow):
         """ Submits workflow
         """
-        unix.cd(PATH.SUBMIT)
+        unix.cd(PATH.WORKDIR)
         if not exists('./scratch'): 
-            unix.ln(PATH.SCRATCH, PATH.SUBMIT+'/'+'scratch')
+            unix.ln(PATH.SCRATCH, PATH.WORKDIR+'/'+'scratch')
 
         unix.mkdir(PATH.OUTPUT)
-        self.checkpoint()
+        workflow.checkpoint()
 
         # prepare sbatch arguments
         call('sbatch '
                 + '%s ' % PAR.SLURMARGS
                 + '--partition=%s ' % PAR.PARTITION
                 + '--job-name=%s ' % PAR.TITLE
-                + '--output %s ' % (PATH.SUBMIT+'/'+'output.log')
+                + '--output %s ' % (PATH.WORKDIR+'/'+'output.log')
                 + '--cpus-per-task=%d '%PAR.NPROC
                 + '--ntasks=%d '%PAR.NTASK
                 + '--time=%d ' % PAR.WALLTIME
